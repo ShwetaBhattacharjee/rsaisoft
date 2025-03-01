5196eda4434aa2bd61bd053d68e272186a2d1878
ShwetaBhattacharjee <shwetabhattacharje@gmail.com>
2025-03-01 22:15:46 +0400

    sitemap.xml added

---
+++ app/sitemap.js
@@ -0,0 +1,17 @@
+const BASE_URL = "https://rsaisoft.vercel.app";
+
+// Function to generate the current date in YYYY-MM-DD format
+function getCurrentDate() {
+  return new Date().toISOString().split("T")[0];
+}
+
+// Default export required for Next.js App Router
+export default function sitemap() {
+  return [
+    {
+      url: `${BASE_URL}/`,
+      lastModified: getCurrentDate(),
+      priority: 1.0,
+    },
+  ];
+}
