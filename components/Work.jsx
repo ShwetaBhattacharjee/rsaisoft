f9c38bab3b3fe12af4c5f37a9ea62165c515e4dd
ShwetaBhattacharjee <shwetabhattacharje@gmail.com>
2025-03-01 10:48:13 +0400

    sitemap.xml added

--- components/Work.jsx
+++ components/Work.jsx
@@ -1,4 +1,4 @@
-import { assets, workData } from '@/assets/assets'
+import { assets, workData } from '../assets/assets'
 import Image from 'next/image'
 import React from 'react'
 import { motion } from "motion/react"
