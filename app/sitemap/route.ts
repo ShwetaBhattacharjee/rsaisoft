32bcc768a5c33870b07b75f13ad12ab7fa0ec903
ShwetaBhattacharjee <85629282+ShwetaBhattacharjee@users.noreply.github.com>
2025-03-14 01:44:53 +0400

    Update route.ts

--- app/sitemap/route.ts
+++ app/sitemap/route.ts
@@ -1,1 +1,19 @@
+export async function GET() {
+  const BASE_URL = "https://www.rsaisoft.info";
+  const currentDate = new Date().toISOString().split("T")[0];
 
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>${BASE_URL}/</loc>
+      <lastmod>${currentDate}</lastmod>
+      <priority>1.0</priority>
+    </url>
+  </urlset>`;
+
+  return new Response(sitemap, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+}
