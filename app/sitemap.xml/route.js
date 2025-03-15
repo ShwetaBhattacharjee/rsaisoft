345e97d027d896bd409ff269e9e8e930cec1a7a7
ShwetaBhattacharjee <85629282+ShwetaBhattacharjee@users.noreply.github.com>
2025-03-15 10:03:01 +0400

    Update route.js

--- app/sitemap.xml/route.js
+++ app/sitemap.xml/route.js
@@ -1,13 +1,12 @@
-
 export async function GET() {
   const BASE_URL = "https://rsinfo.info"; // Ensure this is correct
-  const currentDate = new Date().toISOString().split("T")[0];
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
       <loc>${BASE_URL}/</loc>
-      <lastmod>${currentDate}</lastmod>
+      <lastmod>${new Date().toISOString()}</lastmod>
+      <changefreq>daily</changefreq>
       <priority>1.0</priority>
     </url>
   </urlset>`;
